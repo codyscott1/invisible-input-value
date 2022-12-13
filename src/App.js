92847885f60f8b65df8d904dc6073251a0fafebd
Cody Crandall <cody@halfhelix.com>
2022-12-13 16:48:34 -0600

    add example illustrating issue

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
         <input
           type="text"
           placeholder=" "
-          className="text-[16px] border-2 peer h-12 text-white selection:bg-[#fffff1]"
+          className="text-[16px] border-2 peer h-12 text-white selection:bg-[#fffff1] w-48"
           onChange={handleChange}
           value={value}
           ref={inputRef}
         />
         <span className="text-[8px] absolute left-1 top-0 peer-placeholder-shown:top-1 peer-placeholder-shown:text-[12px] transition-all">
-          Placeholder
+          This will not zoom on ios
         </span>
         <span
           className="absolute top-3 left-1 text-[12px] pointer-events-none"
@@ -27,6 +27,11 @@ function App() {
           {value}
         </span>
       </div>
+      <input
+        type="text"
+        className="text-[12px] h-12 border-2 mt-8 w-48"
+        placeholder="This will zoom your IOS device"
+      />
     </div>
   );
 }
